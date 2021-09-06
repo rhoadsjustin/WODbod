675d3d836c3bbdd56c9489540f4935addef19e98
Justin Rhoads <rhoadsjustin@gmail.com>
2021-09-06 09:53:38 -0400

    Initial Commit

---
+++ Shared/WODbodApp.swift
@@ -0,0 +1,17 @@
+//
+//  WODbodApp.swift
+//  Shared
+//
+//  Created by Justin Rhoads on 9/6/21.
+//
+
+import SwiftUI
+
+@main
+struct WODbodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
